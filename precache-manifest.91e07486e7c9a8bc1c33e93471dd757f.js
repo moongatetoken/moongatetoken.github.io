6f3343f4927b3d62f5523f7ebed852cdfa5e2454
Jakub Kawalec <jakub.kawalec@yahoo.com>
2021-03-28 14:06:12 -0700

    initial

---
+++ precache-manifest.91e07486e7c9a8bc1c33e93471dd757f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5cd7aed492a45f0627d02875af510c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14df5b12bb49222c8a38",
+    "url": "/static/css/main.17de3759.chunk.css"
+  },
+  {
+    "revision": "86dab883a08ff7fd076f",
+    "url": "/static/js/2.1d00a4f9.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.1d00a4f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14df5b12bb49222c8a38",
+    "url": "/static/js/main.ccb0c120.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "e57060c25f072ef7e187bdf772c79f1d",
+    "url": "/static/media/astro.e57060c2.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "6731245ccf784dd95bd349b88316d5b6",
+    "url": "/static/media/dayone.6731245c.jpeg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "151e582d32f4e12a4dd601fa9a5c098a",
+    "url": "/static/media/gorilla.151e582d.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "37d3bc5ace189b993ec5939578bd0e4c",
+    "url": "/static/media/monkeycool.37d3bc5a.jpeg"
+  }
+]);
