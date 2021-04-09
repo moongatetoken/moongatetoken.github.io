6fe690d4c7cb9634c48f8fcb4e28ab871e515e95
Jakub Kawalec <jakub.kawalec@yahoo.com>
2021-04-09 10:39:23 -0700

    update hodlers

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eb8d1fdb1e72b25d6ae260759dceeae0.js"
+  "/precache-manifest.9eeb03da2802fa4e57d2a95048ae316a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
