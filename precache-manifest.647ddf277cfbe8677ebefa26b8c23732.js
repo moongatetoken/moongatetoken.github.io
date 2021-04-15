b52b3adc68986a0f1cb1d8cb89ecda75073cb69d
Jakub Kawalec <jakub.kawalec@yahoo.com>
2021-04-15 11:10:33 -0700

    add winner

---
+++ precache-manifest.647ddf277cfbe8677ebefa26b8c23732.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed9028d21df892fc222b3e72aac094e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d20a0a47b97fe6fc4689",
+    "url": "/static/css/main.d8469d5c.chunk.css"
+  },
+  {
+    "revision": "f323249894bb20c5ad7d",
+    "url": "/static/js/2.7522bb65.chunk.js"
+  },
+  {
+    "revision": "701b6fa046531a99731bcd10d1c98495",
+    "url": "/static/js/2.7522bb65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d20a0a47b97fe6fc4689",
+    "url": "/static/js/main.9ffe69ae.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "80b684b141fe4049d016afa42c292307",
+    "url": "/static/media/HZN_03.80b684b1.png"
+  },
+  {
+    "revision": "9611585acbd010f0e04b616f5da9f15c",
+    "url": "/static/media/city.9611585a.png"
+  },
+  {
+    "revision": "23600d80b765fbadb80d208adb6abbbb",
+    "url": "/static/media/coin_hzn.23600d80.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0be6c041c9d267c91e0def22b65ca0c3",
+    "url": "/static/media/game.0be6c041.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "7cf699470ee617f75112da66bcabcf16",
+    "url": "/static/media/roadmap.7cf69947.jpg"
+  },
+  {
+    "revision": "40d40ed0720047dafeb2a66d8cd3e5ba",
+    "url": "/static/media/ticket.40d40ed0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d373430d7d507c90e9da280afabe79f2.js"
+  "/precache-manifest.647ddf277cfbe8677ebefa26b8c23732.js"
 );
 
 self.addEventListener('message', (event) => {
