63a3049aaf1ac2ad565f130f8a33fe83c16c7197
Jakub Kawalec <jakub.kawalec@yahoo.com>
2021-04-25 16:41:10 -0700

    add manual registery

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f695e426e8d78353095f16c6c176ac77.js"
+  "/precache-manifest.3567d6347503c109cd691ed7dec7ebbf.js"
 );
 
 self.addEventListener('message', (event) => {
