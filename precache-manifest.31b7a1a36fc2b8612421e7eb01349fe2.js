5727cae90d18368224206df446bdce0494a7fade
Jakub Kawalec <jakub.kawalec@yahoo.com>
2021-04-29 15:26:21 -0700

    rebrand

---
+++ precache-manifest.31b7a1a36fc2b8612421e7eb01349fe2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5105e93f4af2f6a50f98f126405dcc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8a8ed901778f832716d",
+    "url": "/static/css/main.82a04d6c.chunk.css"
+  },
+  {
+    "revision": "60b896d86852d9180bd6",
+    "url": "/static/js/2.4306f792.chunk.js"
+  },
+  {
+    "revision": "701b6fa046531a99731bcd10d1c98495",
+    "url": "/static/js/2.4306f792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8a8ed901778f832716d",
+    "url": "/static/js/main.69cc9a1e.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "0bc0e7d337899f8ab69f5311eb880e94",
+    "url": "/static/media/HZN_main.0bc0e7d3.png"
+  },
+  {
+    "revision": "5c2098cb7771c19ae52b0ac27dd921e5",
+    "url": "/static/media/asdf.5c2098cb.png"
+  },
+  {
+    "revision": "4b6ebf6a91f93bf5846d79faff5ac087",
+    "url": "/static/media/bronze.4b6ebf6a.png"
+  },
+  {
+    "revision": "23600d80b765fbadb80d208adb6abbbb",
+    "url": "/static/media/coin_hzn.23600d80.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0be6c041c9d267c91e0def22b65ca0c3",
+    "url": "/static/media/game.0be6c041.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "7cf699470ee617f75112da66bcabcf16",
+    "url": "/static/media/roadmap.7cf69947.jpg"
+  },
+  {
+    "revision": "77fdfc84a256bf7dad6edf8dabbcb0ca",
+    "url": "/static/media/silver.77fdfc84.png"
+  },
+  {
+    "revision": "40d40ed0720047dafeb2a66d8cd3e5ba",
+    "url": "/static/media/ticket.40d40ed0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5ab3853ad59ee234199d0bfa21748f14.js"
+  "/precache-manifest.31b7a1a36fc2b8612421e7eb01349fe2.js"
 );
 
 self.addEventListener('message', (event) => {
